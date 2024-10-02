8ef7fa52a910514f212fe2fc60ae90fad5f0c9ed
XiaoXiong <xiaoxiong.xx21@gmail.com>
2024-10-02 09:41:11 +0800

    add some comments

--- dataset/datasetMAPF.py
+++ dataset/datasetMAPF.py
@@ -48,7 +48,7 @@ class MAPFDataset(Dataset):
         self.train_data = torch.FloatTensor(self.train_data)
         self.action_data = torch.from_numpy(data['action_data'])
         self.action_data = torch.FloatTensor(self.action_data)
-        # 没有mask信息吗
+        
         
     def parallel_load_data(self, yaml_files):
         """
