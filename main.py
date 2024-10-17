ad0565d60049104fcd4de2967bf1ea4d52019b3f
XiaoXiong <xiaoxiong.xx21@gmail.com>
2024-10-17 15:02:49 +0800

    train

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from copy import deepcopy as dp
 from args import get_args
 from models.unet import UNet
 from dataset.datasetMAPF import MAPFDataset
+from evaluation import evaluate
 from tqdm import tqdm
 
 def train(args, model, train_loader, optimizer, loss_fn, device='cuda'):
@@ -53,7 +54,9 @@ def train(args, model, train_loader, optimizer, loss_fn, device='cuda'):
             optimizer.step()
 
             epoch_loss += loss.item()
-
+        test_loss = evaluate(args, model)
+        model.train()
+        print(epoch_loss, test_loss)
         print(f"Epoch {epoch}/{args.epochs}, Loss: {epoch_loss}")
 
 
@@ -72,15 +75,18 @@ if __name__ == "__main__":
     net = UNet(n_channels=feature_channels, n_classes=args.action_dim, bilinear=False)
     # net.use_checkpointing() 
     optimizer = torch.optim.RMSprop(net.parameters(),
-                              lr=args.lr, weight_decay=1e-5, momentum=0.999, foreach=True)
+                              lr=args.lr, weight_decay=1e-8, momentum=0.999, foreach=True)
     loss_fn = nn.CrossEntropyLoss(reduction="none")  #  reduction="none"，因此每个样本的损失都保留为独立值(不进行取平均之类的操作。结合掩码（mask）来只计算某些特定样本的损失，或 reduction="none"，因此每个样本的损失都保留为独立值。这个设计通常用于后续进行某些自定义操作，比如结合掩码（mask）来只计算某些特定样本的损失。
 
     
     # dataset 
-    train_data = MAPFDataset(args.dataset_path, args.feature_dim)  # input shape: (num_samples, 21, 32, 32) action output shape: (num_samples, 32, 32)
+    train_data = MAPFDataset(args.dataset_path, args.agent_dim)  # input shape: (num_samples, 21, 32, 32) action output shape: (num_samples, 32, 32)
     train_loader = DataLoader(train_data, shuffle=True,  
                               batch_size=args.batch_size, 
                               num_workers=1)
     
     # train
     train(args, net, train_loader)
+    
+    ### 到时候数据需要去掉所有重复的
+    
