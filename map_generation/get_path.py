071e224913e5c0f6eefe74511add1b0754f9f0bb
Jingyi Xi <jingyi_xi@zju.edu.cn>
2024-11-17 21:25:38 +0800

    debug

--- map_generation/get_path.py
+++ map_generation/get_path.py
@@ -1,18 +1,12 @@
 from pogema_toolbox.eval_utils import save_evaluation_results
+from pogema_toolbox.evaluator import evaluation
+from pogema_toolbox.registry import ToolboxRegistry
+from pogema_toolbox.create_env import Environment
 
-from pathlib import Path
+from lacam.inference import LacamInference, LacamInferenceConfig
+from create_env import create_env_base
 
 import yaml
-from pogema_toolbox.evaluator import evaluation
-
-from pogema_toolbox.registry import ToolboxRegistry
-
-from create_env import create_env_base
-from pogema_toolbox.create_env import Environment
-from lacam.inference import LacamInference, LacamInferenceConfig
-
-
-BASE_PATH = Path('map_generation')
 
 def main():
     env_cfg_name = 'Environment'
@@ -26,18 +20,18 @@ def main():
     ]
 
     for folder in folder_names:
-        maps_path = BASE_PATH / folder / "maps.yaml"
+        maps_path = folder / "maps.yaml"
         print(f"Loading maps from {maps_path}")
         with open(maps_path, 'r') as f:
             maps = yaml.safe_load(f)
         ToolboxRegistry.register_maps(maps)
         
-        config_path = BASE_PATH / folder / "config.yaml"
+        config_path = folder / "config.yaml"
         print(f"Loading config from {config_path}")
         with open(config_path) as f:
             evaluation_config = yaml.safe_load(f)
         
-        eval_dir = BASE_PATH / folder
+        eval_dir = folder
         print(f"Running evaluation for {eval_dir}")
         evaluation(evaluation_config, eval_dir=eval_dir)
         save_evaluation_results(eval_dir)
