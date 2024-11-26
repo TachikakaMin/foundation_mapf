3232c67891a45360e7c23fdb70269bee8f14b4d3
Xiao Xiong <xiaoxiong.xx21@gmail.com>
2024-11-25 20:23:26 -0800

    add a print("current_goal_distance: )

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def train(args, model, train_loaders, val_loaders, optimizer, loss_fn, device):
                 current_goal_distance, _map, trajectories, goal_positions = path_formation(args, model, val_loaders[i], 0, 0, device, action_choice="sample")
                 animate_paths(args, epoch, trajectories, goal_positions, _map, interval=500)
                 args.writer.add_scalar('Loss/video_goal_dis', current_goal_distance, epoch)
-                print(current_goal_distance)
+                print("current_goal_distance: ", current_goal_distance)
 
         if epoch % args.save_interval == 0:
             file_path = os.path.join(args.real_log_dir, f"model_checkpoint_epoch_{epoch}.pth")
