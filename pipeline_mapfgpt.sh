e44857dd96c1ee3f61bf5656238b86178863c52e
Jingyi Xi <jingyi_xi@zju.edu.cn>
2025-01-25 13:19:40 +0800

    update  pipeline_mapfgpt.sh

--- pipeline_mapfgpt.sh
+++ pipeline_mapfgpt.sh
@@ -21,26 +21,26 @@ for category in 01-random 02-mazes; do
 done | parallel --progress --bar --eta --timeout 10 --colsep ' ' \
     'data_generation_LACAM/lacam/build/main -m {1} -N {2} -s {3} -v 1 -o {4}'
 
-# Generate paths for each map category and map
-for category in 03-warehouse; do
-    # Find all .map files in the category directory
-    find "data_mapf_gpt/map_files/${category}" -name "*.map" | while read map_file; do
-        map_name=$(basename "$map_file" .map)
+# # Generate paths for each map category and map
+# for category in 03-warehouse; do
+#     # Find all .map files in the category directory
+#     find "data_mapf_gpt/map_files/${category}" -name "*.map" | while read map_file; do
+#         map_name=$(basename "$map_file" .map)
         
 
-        # generate 10 paths each
-        for N in 32 64 96 128 160 192; do
-            mkdir -p "data_mapf_gpt/path_files/${category}/${map_name}/${map_name}-${N}"
-            for seed in {0..9}; do
-                output_file="data_mapf_gpt/path_files/${category}/${map_name}/${map_name}-${N}/${map_name}-${N}-${seed}.path"
-                if [ ! -f "$output_file" ]; then
-                    echo "$map_file $N $seed $output_file"
-                fi
-            done
-        done
-    done
-done | parallel --progress --bar --eta --timeout 10 --colsep ' ' \
-    'data_generation_LACAM/lacam/build/main -m {1} -N {2} -s {3} -v 1 -o {4}'
+#         # generate 10 paths each
+#         for N in 32 64 96 128 160 192; do
+#             mkdir -p "data_mapf_gpt/path_files/${category}/${map_name}/${map_name}-${N}"
+#             for seed in {0..9}; do
+#                 output_file="data_mapf_gpt/path_files/${category}/${map_name}/${map_name}-${N}/${map_name}-${N}-${seed}.path"
+#                 if [ ! -f "$output_file" ]; then
+#                     echo "$map_file $N $seed $output_file"
+#                 fi
+#             done
+#         done
+#     done
+# done | parallel --progress --bar --eta --timeout 10 --colsep ' ' \
+#     'data_generation_LACAM/lacam/build/main -m {1} -N {2} -s {3} -v 1 -o {4}'
 
 # Generate paths for each map category and map
 for category in 04-movingai; do
